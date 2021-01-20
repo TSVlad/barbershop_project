dfcec7cb208a5369a85dbcd00acb71f828df5202
TSVlad <vladislav.kolmykov@yandex.ru>
2021-01-20 15:23:48 +0300

    logging add

--- src/main/java/com/tsvlad/barber_project/aop/LoggerAOP.java
+++ src/main/java/com/tsvlad/barber_project/aop/LoggerAOP.java
@@ -1,8 +1,14 @@
 package com.tsvlad.barber_project.aop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+
 public class LoggerAOP {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public void logException(Exception ex) {
-        System.out.println(ex);
+        logger.error(LocalDateTime.now() + " Exception caught: " + ex);
     }
 }
